6e16cfc4352e332742964d6b4b034ca6bd3da72a
Sakurai616 <yyingjing82@gmail.com>
2023-02-24 01:57:54 +0900

    kanniTop画面作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    themes: [
+      "emerald"
+    ],
+  },
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
