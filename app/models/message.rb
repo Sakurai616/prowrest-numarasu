88850230baf658ef1e36ed49fd79fd0991709bc7
Sakurai616 <yyingjing82@gmail.com>
2023-03-28 21:43:15 +0900

    Fix:リアルタイムグループチャット機能

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat_group
 
-  mount_uploader :image, ImageUploader
-
   validates :body, presence: true, length: { maximum: 65_535 }
 
   after_create_commit { MessageBroadcastJob.perform_later self }
