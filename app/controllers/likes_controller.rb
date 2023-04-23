61908242c81ec5192009dab64e0b6374ec31d5a4
Sakurai616 <yyingjing82@gmail.com>
2023-04-24 00:58:19 +0900

    いいね機能ajax化修正 、デザイン調整

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,22 +3,10 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.like(@post)
-    
-    render turbo_stream: turbo_stream.replace(
-      'like',
-      partial: 'posts/like',
-      locals: { post: @post },
-    )
   end
 
   def destroy
     @post = current_user.likes.find(params[:id]).post
     current_user.unlike(@post)
-
-    render turbo_stream: turbo_stream.replace(
-      'like',
-      partial: 'posts/like',
-      locals: { post: @post },
-    )
   end
 end
