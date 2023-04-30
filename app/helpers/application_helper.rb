de2b2daadf740cc2a91d88b0c4354755bbad6883
Sakurai616 <yyingjing82@gmail.com>
2023-04-30 17:35:21 +0900

    メタタグの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,29 @@
 module ApplicationHelper
-  def page_title(page_title = '')
-    base_title = 'プロレスヌマラス'
-    
-    page_title.empty? ? base_title : page_title
+  def default_meta_tags
+    {
+      site: "プロレスヌマラス",
+      title: "プロレスファンに出会いを",
+      reverse: true,
+      separator: '|',
+      description: "プロレスを語りたいプロレスファンの方がクイズ、投稿、グループチャットを通して楽しく、そして熱く交流することができるサービスです。",
+      keywords: "プロレス, プロレスファン, プロレスクイズ",
+      charset: "UTF-8",
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: image_url(''),
+      og: {
+        site_name: "プロレスヌマラス",
+        title: "プロレスファンに出会いを",
+        description: "プロレスを語りたいプロレスファンの方がクイズ、投稿、グループチャットを通して楽しく、そして熱く交流することができるサービスです。",
+        type: 'website',
+        url: request.original_url,
+        image: image_url(''),
+        local: 'ja-JP',
+      },
+      twitter: {
+        card: '',
+        site: '@N7sUjYSKfiBxz47',
+      }
+    }
   end
 end
