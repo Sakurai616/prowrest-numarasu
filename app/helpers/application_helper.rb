763d4460e11727a1982e506000dfbcbded492064
Sakurai616 <yyingjing82@gmail.com>
2023-04-30 21:10:04 +0900

    OGPの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,18 +10,17 @@ module ApplicationHelper
       charset: "UTF-8",
       canonical: request.original_url,
       noindex: !Rails.env.production?,
-      icon: image_url(''),
       og: {
         site_name: "プロレスヌマラス",
         title: "プロレスファンに出会いを",
         description: "プロレスを語りたいプロレスファンの方がクイズ、投稿、グループチャットを通して楽しく、そして熱く交流することができるサービスです。",
         type: 'website',
         url: request.original_url,
-        image: image_url(''),
+        image: image_url('ogp.png'),
         local: 'ja-JP',
       },
       twitter: {
-        card: '',
+        card: 'summary_large_image',
         site: '@N7sUjYSKfiBxz47',
       }
     }
