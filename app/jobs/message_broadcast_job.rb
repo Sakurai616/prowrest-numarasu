0fbb1b48a5dae0ccc361525c17f396794ee1eeef
Sakurai616 <yyingjing82@gmail.com>
2023-05-02 03:10:34 +0900

    リアルタイム通信の実装

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,7 +2,7 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   # ブロードキャスト(一つのネットワークの中にあるすべてのホストに対してデータを送る。)
-  def perform(message, chat_group_id)
+  def perform(message)
     ActionCable.server.broadcast "chat_group_channel_#{message.chat_group_id}", { message: render_message(message) }
   end
 
