9bfb559f2df52740129aaacbe2891ba7e31e8321
Sakurai616 <yyingjing82@gmail.com>
2023-05-15 19:12:35 +0900

    バリデーションエラーの応急処置

--- app/javascript/channels/chat_group_channel.js
+++ app/javascript/channels/chat_group_channel.js
@@ -14,14 +14,20 @@ $(function(){
       return $('#messages').append(data['message']);
     },
 
+    // 仮引数 function(message)のmessage
+    // 実引数 event.target.value
+    // chat_group_channel.rbのspeakアクションを動かすために、speak関数を定義
     speak: function(message) {
       return this.perform('speak', {message: message});
     }
 
   });
 
+  // フォーム内でEnterキーが押された時の動作を記述
+  // event.KeyCode === 13は非推奨となっているため、event.key === 'Enter'と変更
   $(document).on('keypress', '[data-behavior~=chat_group_speaker]', function(event) {
     if (event.key === 'Enter') {
+      console.log(chatChannel);
       chatChannel.speak(event.target.value);
       event.target.value = '';
       return event.preventDefault();
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,7 @@ class Message < ApplicationRecord
   belongs_to :chat_group
 
   validates :body, presence: true, length: { maximum: 65_535 }
-
+  
+  # データ保存後にMessageBroadcastJobのperformメソッドを実行,引数はself
   after_create_commit { MessageBroadcastJob.perform_later self }
 end
