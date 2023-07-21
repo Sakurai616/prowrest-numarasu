7987725de7aaba1f5271b94ad03b0e9a7929136f
Sakurai616 <yyingjing82@gmail.com>
2023-07-22 02:56:35 +0900

    タイトルのアニメーション追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
       animation: {
         'bounce': 'bounce 1s infinite',
         'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        "text-shadow-drop-center": "text-shadow-drop-center 0.6s ease   both",
+        "text-focus-in": "text-focus-in 1s cubic-bezier(0.550, 0.085, 0.680, 0.530)   both"
       },
       keyframes: {
         bounce: {
@@ -15,6 +17,24 @@ module.exports = {
           '0%': { opacity: '1' },
           '50%': { opacity: '.5' },
           '100%': { opacity: '1' },
+        },
+        "text-shadow-drop-center": {
+          "0%": {
+              "text-shadow": "0 0 0 transparent"
+          },
+          to: {
+              "text-shadow": "0 0 18px rgba(0, 0, 0, .35)"
+          }
+        },
+        "text-focus-in": {
+          "0%": {
+              filter: "blur(12px)",
+              opacity: "0"
+          },
+          to: {
+              filter: "blur(0)",
+              opacity: "1"
+          }
         },
       },
     },
